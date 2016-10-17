c1a332c0ffee70d90c8fc72683506ae16ba0a61d
Ian Agpawa <agpawaji@gmail.com>
2016-10-16 21:22:58 -0400

    cursing

--- movie-trailer/square.py
+++ movie-trailer/square.py
@@ -1,5 +1,11 @@
 import turtle
 
+def square(turt):
+    for x in range(0,4):
+        turt.forward(100)
+        turt.right(90)
+
+
 def draw_shapes():
     window = turtle.Screen()
     window.bgcolor('black')
@@ -9,10 +15,13 @@ def draw_shapes():
     donnie.color("green")
     donnie.speed(2)
 
-    for x in range(0, 4):
-        donnie.forward(100)
-        donnie.right(90)
+    for x in range(0, 36):
+        square(donnie)
+        donnie.right(10)
 
+    window.exitonclick()
+
+'''
     amy = turtle.Turtle()
     amy.shape('turtle')
     amy.color("pink")
@@ -26,6 +35,8 @@ def draw_shapes():
         kingsley.back(120)
         kingsley.left(120)
 
-    window.exitonclick()
+'''
+
+
 
 draw_shapes()
