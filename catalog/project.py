a4eed7bb7efea600ac24756df37e76a4ba8e0065
Ian Agpawa <agpawaji@gmail.com>
2016-11-30 20:53:28 -0500

    form requests

--- catalog/project.py
+++ catalog/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -13,36 +13,28 @@ session = DBSession()
 
 
 @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/')
 def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+    return "Hello World!"
+
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = ''
-    for item in items:
-        output += item.name
-        output += '</br>'
-        output += item.price
-        output += '</br>'
-        output += item.description
-        output += '</br></br>'
-    return output
+    return render_template('menu.html', restaurant = restaurant, items = items)
 
 
 # Task 1: Create route for newMenuItem function here
-
-
+@app.route('/restaurants/<int:restaurant_id>/new/', methods = ['GET', 'POST'])
 def newMenuItem(restaurant_id):
     return "page to create a new menu item. Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
-
-
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
 
 # Task 3: Create a route for deleteMenuItem function here
-
-
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
 
