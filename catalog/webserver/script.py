152b83434e43a081b122465141208295c6baf088
Ian Agpawa <agpawaji@gmail.com>
2016-11-30 20:07:18 -0500

    framework

---
+++ catalog/webserver/script.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+
+session = DBSession()
+#
+# myFirstRestaurant = Restaurant(name = "Pizza Palace")
+#
+# session.add(myFirstRestaurant)
+# session.commit()
+#
+#
+# cheesepizza = MenuItem(name = "Cheese Pizza",
+#                         description = "Made with all natural ingredients and fresh mozzarella",
+#                         course = "Entree", price = "$8.99",
+#                         restaurant = myFirstRestaurant)
+#
+# session.add(cheesepizza)
+# session.commit()
+
+items = session.query(Restaurant).all()
+for item in items:
+    print item.name
