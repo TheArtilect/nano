03100ce4ce658357fed375b60d89065da75cdd2d
Ian Agpawa <agpawaji@gmail.com>
2016-12-12 00:34:45 -0500

    Premium cats

--- map/moving/js/script.js
+++ map/moving/js/script.js
@@ -1,81 +1,85 @@
+function displayAll(){
+    function loadData() {
 
-function loadData() {
+        var $body = $('body');
+        var $wikiElem = $('#wikipedia-links');
+        var $nytHeaderElem = $('#nytimes-header');
+        var $nytElem = $('#nytimes-articles');
+        var $greeting = $('#greeting');
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+        // clear out old data before new request
+        $wikiElem.text("");
+        $nytElem.text("");
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
-
-    // load streetview
-    var streetIn = $('#street').val();
-    var cityIn = $('#city').val();
-    var cityW = (cityIn.split(' ')).join("%20")
-    var loc = streetIn + ",_" + cityIn;
-    var url = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=%LOC%"
-    var source = url.replace("%LOC%", loc)
-    console.log(source)
-
-
-
-    $('body').append("<img class='bgimg' id='picture' src='" + source + "'>");
-    // YOUR CODE GOES HERE!
-
-    var urlLoc = ((streetIn + " " + cityIn).split(" ")).join("+")
-    var apiKey = "56444d2c06dd4e9aba0fefdcb12077da"
-
-    var article = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + "?api-key=" + apiKey + "&q=" + urlLoc + "&sort=newest";
+        // load streetview
+        var streetIn = $('#street').val();
+        var cityIn = $('#city').val();
+        var cityW = (cityIn.split(' ')).join("%20")
+        var loc = streetIn + ",_" + cityIn;
+        var url = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=%LOC%"
+        var source = url.replace("%LOC%", loc)
+        
 
 
-  // $("#test").html(article)
 
-    $.getJSON(article, function(data){
-      $("#nytimes-header").text("NYT Articles About" + streetIn + ', ' + cityIn);
-      articles = data.response.docs;
-      for (var i = 0; i < articles.length; i++) {
-        var article = articles[i];
-        $("#nytimes-articles").append(
-          "<li class='article'>" +
-          "<a target='_blank' href='" + article.web_url + "'>" + article.headline.main + "</a>" +
-          "<p>" + article.snippet + "</p>" +
-          "</li>");
-      };
-    }).error(function() {
-      $("#nytimes-header").text("New York Times Articles Could Not Be Loaded");
-    })
+        $('body').append("<img class='bgimg' id='picture' src='" + source + "'>");
+        // YOUR CODE GOES HERE!
+
+        var urlLoc = ((streetIn + " " + cityIn).split(" ")).join("+")
+        var apiKey = NYTAPI
+
+        var article = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + "?api-key=" + apiKey + "&q=" + urlLoc + "&sort=newest";
 
 
-    var addy = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + cityIn + "&format=json&callback=wikiCallback";
+      // $("#test").html(article)
+
+        $.getJSON(article, function(data){
+          $("#nytimes-header").text("NYT Articles About" + streetIn + ', ' + cityIn);
+          articles = data.response.docs;
+          for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $("#nytimes-articles").append(
+              "<li class='article'>" +
+              "<a target='_blank' href='" + article.web_url + "'>" + article.headline.main + "</a>" +
+              "<p>" + article.snippet + "</p>" +
+              "</li>");
+          };
+        }).error(function() {
+          $("#nytimes-header").text("New York Times Articles Could Not Be Loaded");
+        })
 
 
-    	// $("#wikipedia-header").html(addy);
-
-	var wikiRequestTimeout = setTimeout(function(){
-		$("#wikipedia-links").text("failed to get wikipedia resources");
-	}, 8000);
-
-	$.ajax({
-		url: addy,
-		dataType: 'jsonp',
-		success: function(wiki) {
-			for (var i = 0; i < wiki[1].length; i++){
-				var headline = wiki[1][i];
-				var linkA = 'http://en.wikipedia.org/wiki/' + wiki[1][i];
-				$("#wikipedia-links").append("<li>" +
-    				"<a href='" + linkA + "'>" + headline + "</a>" +
-    				"</li>")
-			};
-
-			clearTimeout(wikiRequestTimeout);
-		}
-	});
+        var addy = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + cityIn + "&format=json&callback=wikiCallback";
 
 
-    return false;
-};
+        	// $("#wikipedia-header").html(addy);
 
-$('#form-container').submit(loadData);
+    	var wikiRequestTimeout = setTimeout(function(){
+    		$("#wikipedia-links").text("failed to get wikipedia resources");
+    	}, 8000);
+
+    	$.ajax({
+    		url: addy,
+    		dataType: 'jsonp',
+    		success: function(wiki) {
+    			for (var i = 0; i < wiki[1].length; i++){
+    				var headline = wiki[1][i];
+    				var linkA = 'http://en.wikipedia.org/wiki/' + wiki[1][i];
+    				$("#wikipedia-links").append("<li>" +
+        				"<a href='" + linkA + "'>" + headline + "</a>" +
+        				"</li>")
+    			};
+
+    			clearTimeout(wikiRequestTimeout);
+    		}
+    	});
+
+
+        return false;
+    };
+
+    $('#form-container').submit(loadData);
+}
+
+$(document).ready(displayAll);
+$(document).on('page:load', displayAll);
--- map/neighborhood/js/script.js
+++ map/neighborhood/js/script.js
@@ -1,11 +1,19 @@
 
 function startPage() {
-    count = 0
-
-    $("#cat-pic").on('click', function(){
-        count += 1
-        $("#counter").html("Clicks: %COUNT%".replace('%COUNT%', count));
+    var count_1 = 0
+    var count_2 = 0
+    $("#name-1").html("Dr. StrangeHate")
+    $("#name-2").html("Chairman Meow")
+    $("#cat-pic-1").on('click', function(){
+        count_1 += 1
+        $("#counter-1").html("Clicks: %COUNT%".replace('%COUNT%', count_1));
     })
+    $("#cat-pic-2").on('click', function(){
+        count_2 += 1
+        $("#counter-2").html("Clicks: %COUNT%".replace('%COUNT%', count_2));
+    })
+
+
 
 };
 
