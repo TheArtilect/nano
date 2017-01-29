9ce6eb0d4d038b5193cb52c08ab050c30b0b62d2
Ian Agpawa <agpawaji@gmail.com>
2017-01-29 02:58:28 -0500

    add: Recursion

---
+++ technical/search/binary_search_practice.py
@@ -0,0 +1,59 @@
+'''
+THIS IS RECURSIVE
+def binarySearch(alist, item):
+    if len(alist) == 0:
+        return False
+    else:
+        midpoint = len(alist)//2
+        if alist[midpoint]==item:
+            return True
+        else:
+            if item<alist[midpoint]:
+                return binarySearch(alist[:midpoint],item)
+            else:
+                return binarySearch(alist[midpoint+1:],item)
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(binarySearch(testlist, 3))
+print(binarySearch(testlist, 13))
+'''
+'''
+def binarySearch(alist, item):
+    first = 0
+    last = len(alist)-1
+    found = False
+
+    while first<=last and not found:
+        midpoint = (first + last)//2
+        if alist[midpoint] == item:
+            found = True
+        else:
+            if item < alist[midpoint]:
+                last = midpoint-1
+            else:
+                first = midpoint+1
+
+    return found
+'''
+
+"""You're going to write a binary search function.
+You should use an iterative approach - meaning
+using loops.
+Your function should take two inputs:
+a Python list to search through, and the value
+you're searching for.
+Assume the list only has distinct elements,
+meaning there are no repeated values, and
+elements are in a strictly increasing order.
+Return the index of value, or -1 if the value
+doesn't exist in the list."""
+
+def binary_search(input_array, value):
+    midpoint = len(input_array) / 2
+
+
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print binary_search(test_list, test_val1)
+print binary_search(test_list, test_val2)
