eef60467354fe60a63e4f9045000b5a5298f4fd9
Ian Agpawa <agpawaji@gmail.com>
2017-02-01 22:33:20 -0500

    add: no spaces palindrome

--- technical/mock/palindrome.py
+++ technical/mock/palindrome.py
@@ -1,19 +1,34 @@
+#
+# def is_palindrome(string):
+#     mid = len(string) / 2
+#     if len(string) % 2 == 0:
+#         left_start = mid - 1
+#         right_start = mid
+#
+#     else:
+#         right_start = mid + 1
+#         left_start = mid - 1
+#
+#     for x in range(mid):
+#
+#         if string[left_start - x] != string[right_start + x]:
+#            return False
+#     return True
 
 def palindrome(string):
-    mid = len(string) / 2
-    if len(string) % 2 == 0:
-        left_start = mid - 1
-        right_start = mid
-
-    else:
-        right_start = mid + 1
-        left_start = mid - 1
-
-    for x in range(mid):
-
-        if string[left_start - x] != string[right_start + x]:
-           return False
+    x = 0
+    y = -1
+    for x in range(len(string) / 2):
+        if string[x] != string[y]:
+            return False
+        x += 1
+        y -= 1
     return True
+
+
+def with_spaces(string):
+    no_space_string = "".join(string.split(" "))
+    return palindrome(no_space_string)
 
 print palindrome("franklin")
 print palindrome("racecar")
