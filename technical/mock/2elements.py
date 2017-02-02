b06a8f34d61e5baadd15b64f7c7a91250b4efe54
Ian Agpawa <agpawaji@gmail.com>
2017-02-02 00:13:36 -0500

    add: 2elements

--- technical/mock/2elements.py
+++ technical/mock/2elements.py
@@ -13,3 +13,19 @@
 #                 y += 1
 #         x += 1
 #     return False
+
+def adding_up(array, k):
+    table = {}
+    for n in array:
+        difference = k - n
+        if not(n in table):
+            table[n] = difference
+        if difference in table:
+            print difference
+            return True
+    return False
+
+array = [1,4,8,8]
+k = 10
+
+print adding_up(array, k)
